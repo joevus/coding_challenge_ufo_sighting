e61ba5ee81f3d79a171247aff7a691f0005282e4
joevus <joehoskisson@gmail.com>
2019-12-31 16:20:21 -0700

    modify schema, change endpoint.

--- schemadump.sql
+++ schemadump.sql
@@ -4,17 +4,20 @@ USE `test`;
 # Replace this with your schema :)
 #
 
-# Dump of table Car
+# Dump of table Sightings
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `car`;
+DROP TABLE IF EXISTS `sightings`;
 
-CREATE TABLE `car` (
+CREATE TABLE `sightings` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `make` varchar(50) NOT NULL DEFAULT '',
-  `model` varchar(50) NOT NULL DEFAULT '',
-  `year` int(4) NOT NULL,
+  `date` varchar(50) NOT NULL DEFAULT '',
+  `shape` varchar(50) NOT NULL DEFAULT '',
+  `duration` int(11) NOT NULL,
+  `comments` text NOT NULL,
+  `city` text NOT NULL,
+  `state` text NOT NULL,
+  `latitude` varchar(50) NOT NULL,
+  `longitude` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
